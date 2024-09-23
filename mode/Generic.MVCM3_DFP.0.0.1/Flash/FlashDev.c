be1971aaefc143abcf5da476563329394df6ecfb
KuaileNiuma <1600381370@qq.com>
2024-09-23 15:40:32 +0800

    2024.9.23

--- mode/Generic.MVCM3_DFP.0.0.1/Flash/FlashDev.c
+++ mode/Generic.MVCM3_DFP.0.0.1/Flash/FlashDev.c
@@ -27,19 +27,16 @@
 
 struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "New Device 256kB Flash",   // Device Name 
+   "MVCM3110/250 Flash",       // Device Name 
    ONCHIP,                     // Device Type
    0x00000000,                 // Device Start Address
-   0x00040000,                 // Device Size in Bytes (256kB)
+   0x00004000,                 // Device Size in Bytes (16kB)
    1024,                       // Programming Page Size
    0,                          // Reserved, must be 0
    0xFF,                       // Initial Content of Erased Memory
    100,                        // Program Page Timeout 100 mSec
    3000,                       // Erase Sector Timeout 3000 mSec
-
 // Specify Size and Address of Sectors
-   0x002000, 0x000000,         // Sector Size  8kB (8 Sectors)
-   0x010000, 0x010000,         // Sector Size 64kB (2 Sectors) 
-   0x002000, 0x030000,         // Sector Size  8kB (8 Sectors)
+   0x002000, 0x000000,         // Sector Size  8kB (2 Sectors)
    SECTOR_END
 };
---
+++ mode/Generic.MVCM3_DFP.0.0.1/Flash/Objects/flashprg.d
@@ -0,0 +1,1 @@
+./objects/flashprg.o: FlashPrg.c FlashOS.H
